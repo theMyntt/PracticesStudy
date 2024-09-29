5dc489aff93ddf99b4d5e1f97001594a8d5f2b11
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-29 17:05:44 -0300

    chore: table and columns name

--- src/Patterns.Infra.Data/EntitiesConfiguration/CarConfiguration.cs
+++ src/Patterns.Infra.Data/EntitiesConfiguration/CarConfiguration.cs
@@ -8,10 +8,19 @@ public class CarConfiguration : IEntityTypeConfiguration<Car>
 {
     public void Configure(EntityTypeBuilder<Car> builder)
     {
+        builder.ToTable("TBL_CARROS");
+        
         builder.HasKey(model => model.Id);
 
+        builder.Property(model => model.Corp).HasColumnName("TX_MARCA");
         builder.Property(model => model.Corp).IsRequired();
+        
+        builder.Property(model => model.Model).HasColumnName("TX_MODELO");
         builder.Property(model => model.Model).IsRequired();
+        
+        builder.Property(model => model.CreatedAt).HasColumnName("TX_CRIADO_EM");
         builder.Property(model => model.CreatedAt).IsRequired();
+        
+        builder.Property(model => model.UpdatedAt).HasColumnName("TX_ATUALIZADO_EM");
     }
 }
