1dd971e9dc82404814f1b23aeb39a7518439423d
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-29 19:19:49 -0300

    chore: injecting dependencies

--- src/Patterns.Web/Program.cs
+++ src/Patterns.Web/Program.cs
@@ -1,3 +1,5 @@
+using Patterns.Infra.Ioc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddExtensions();
 
 var app = builder.Build();
 
