1c536fbddf5d229f71fe7170aac4692b0c601561
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-09-29 20:56:31 -0300

    chore: injecting create car usecase

--- src/Patterns.Infra.Ioc/Modules/ApplicationExtensions.cs
+++ src/Patterns.Infra.Ioc/Modules/ApplicationExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Patterns.Application.Abstractions;
 using Patterns.Application.Mappers;
+using Patterns.Application.UseCases.Car;
 
 namespace Patterns.Infra.Ioc.Modules;
 
@@ -9,6 +10,7 @@ internal static class ApplicationExtensions
     internal static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ICarMapper, CarMapper>();
+        services.AddScoped<ICreateCarUseCase, CreateCarUseCase>();
         
         return services;
     }
