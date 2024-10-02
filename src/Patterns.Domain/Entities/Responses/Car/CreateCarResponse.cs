da519ec4e087a1b74f089b950d71e0e118086be8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-01 21:38:53 -0300

    chore: create car response entity

---
+++ src/Patterns.Domain/Entities/Responses/Car/CreateCarResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns.Domain.Entities.Responses.Car
+{
+    public class CreateCarResponse : StandardResponse
+    {
+        public Guid Id { get; init; }
+    }
+}
