e3a5df0be79950440f8998d3e4e03ea09e75e83a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-01 21:49:25 -0300

    chore: unique model migrations

--- src/Patterns.Infra.Data/Migrations/20241002004909_UniqueModel.Designer.cs
+++ src/Patterns.Infra.Data/Migrations/20241002004909_UniqueModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Patterns.Infra.Data.Context;
 
@@ -11,9 +12,11 @@ using Patterns.Infra.Data.Context;
 namespace Patterns.Infra.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241002004909_UniqueModel")]
+    partial class UniqueModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Patterns.Infra.Data/Migrations/20241002004909_UniqueModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Patterns.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "TBL_CARROS",
+                newName: "ID_CARRO");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TX_MODELO",
+                table: "TBL_CARROS",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBL_CARROS_TX_MODELO",
+                table: "TBL_CARROS",
+                column: "TX_MODELO",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TBL_CARROS_TX_MODELO",
+                table: "TBL_CARROS");
+
+            migrationBuilder.RenameColumn(
+                name: "ID_CARRO",
+                table: "TBL_CARROS",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TX_MODELO",
+                table: "TBL_CARROS",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
