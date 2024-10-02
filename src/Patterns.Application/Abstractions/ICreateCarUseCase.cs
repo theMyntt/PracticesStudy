eb2abbeb50a10d1c662ee20a9048b541758a8df9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-01 21:48:51 -0300

    fix: tsuccess type on icreatecarusecase

--- src/Patterns.Application/Abstractions/ICreateCarUseCase.cs
+++ src/Patterns.Application/Abstractions/ICreateCarUseCase.cs
@@ -1,9 +1,10 @@
 using Patterns.Application.DTOs.Car;
 using Patterns.Domain.Entities;
+using Patterns.Domain.Entities.Responses.Car;
 
 namespace Patterns.Application.Abstractions;
 
-public interface ICreateCarUseCase : IUseCase<StandardResponse, StandardResponse, CreateCarDTO>
+public interface ICreateCarUseCase : IUseCase<CreateCarResponse, StandardResponse, CreateCarDTO>
 {
     
 }
