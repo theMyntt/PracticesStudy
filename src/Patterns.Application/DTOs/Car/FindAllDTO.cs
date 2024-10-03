1b800880e3ad94f394b2cbaad16e69c787c26371
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-02 21:47:57 -0300

    chore: required annotation to record

--- src/Patterns.Application/DTOs/Car/FindAllDTO.cs
+++ src/Patterns.Application/DTOs/Car/FindAllDTO.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Patterns.Application.DTOs.Car
 {
-    public record FindAllDTO(int page, int limit);
+    public record FindAllDTO
+    {
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Page must be greater than 0")]
+        public int page { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Limit must be between 1 and 100")]
+        public int limit { get; set; }
+    };
 }
