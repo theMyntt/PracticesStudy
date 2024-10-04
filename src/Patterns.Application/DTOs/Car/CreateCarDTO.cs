cf25e97f1c96004f64812e6fdb62615bc7081a9b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-10-04 13:55:28 -0300

    chore: corp & model is required

--- src/Patterns.Application/DTOs/Car/CreateCarDTO.cs
+++ src/Patterns.Application/DTOs/Car/CreateCarDTO.cs
@@ -1,3 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Patterns.Application.DTOs.Car;
 
-public record CreateCarDTO(string Corp, string Model);
+public record CreateCarDTO(
+    [Required] string Corp, 
+    [Required] string Model);
